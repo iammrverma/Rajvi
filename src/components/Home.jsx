5f65de8b41fd689da770ba45571201c41f8daf3d
Raj Verma <rajwebz2020@gmail.com>
2025-01-10 23:22:54 +0530

    Introducing ReusableComponent.jsx, improving CllientsCarousla.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,14 +3,17 @@ import Hero from "./sections/Hero";
 import ClientsCarousal from "./ClientsCarousal";
 import FAQ from "./Faq";
 import Footer from "./Footer";
+import { ReusableSection } from "./ReusableSection";
 
 const Home = () => {
-  return <>
-  <Hero />
-  <ClientsCarousal />
-  <FAQ />
-  <Footer />
-  </>
+  return (
+    <>
+      <ReusableSection children={<Hero />} styles={{marginBottom:"5rem"}}/>
+      <ReusableSection children={<ClientsCarousal />} styles={{marginBottom:"5rem"}} />
+      <ReusableSection children={<FAQ />} styles={{marginBottom:"5rem"}} />
+      <ReusableSection children={<Footer />} />
+    </>
+  );
 };
 
 export default Home;
---
+++ src/components/ReusableSection.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Box } from "@mui/material";
+
+export const ReusableSection = ({ children, styles = {}, className = "" }) => {
+  return (
+    <Box
+      className={className}
+      sx={{
+        padding: "2rem 0",
+        ...styles, // Merge custom styles
+      }}
+    >
+      {children}
+    </Box>
+  );
+};
